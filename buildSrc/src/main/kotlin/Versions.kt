06e563297f806e1ca1a66a1c7f024c080ea0e287
Jeppe Leth <jeppe.leth@gmail.com>
2023-09-13 11:08:51 +0200

    update kotlin and compose versions to latest

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
     const val compile_sdk_version = 34
 
-    const val library_version = "1.0.5"
+    const val library_version = "1.0.6"
 
     const val gradle_version = "7.4.2"
-    const val kotlin_version = "1.9.0"
+    const val kotlin_version = "1.9.10"
 
     const val ktx_version = "1.12.0"
 
@@ -12,8 +12,8 @@ object Versions {
 
     const val coroutine_version = "1.7.1"
 
-    const val compose_compiler_version = "1.5.2"
-    const val compose_version = "1.5.0"
+    const val compose_compiler_version = "1.5.3"
+    const val compose_version = "1.5.1"
     const val compose_activity_version = "1.7.2"
     const val compose_lifecycle_runtime_version = "2.6.2"
 
