5659d1f9366a9152eeb3121a9d860c921364a0a6
Frederik J <>
2023-10-02 09:51:20 +0200

    [Fix] ViewModels not cleaned up - Removed references to saved state on screen

--- core/src/main/java/cafe/adriel/voyager/navigator/core/AndroidScreenLifecycleOwner.kt
+++ core/src/main/java/cafe/adriel/voyager/navigator/core/AndroidScreenLifecycleOwner.kt
@@ -1,89 +1,36 @@
 package cafe.adriel.voyager.navigator.core
 
 import android.app.Activity
-import android.app.Application
 import android.content.Context
-import android.content.ContextWrapper
-import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidedValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
-import androidx.lifecycle.DEFAULT_ARGS_KEY
-import androidx.lifecycle.HasDefaultViewModelProviderFactory
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
-import androidx.lifecycle.SAVED_STATE_REGISTRY_OWNER_KEY
-import androidx.lifecycle.SavedStateViewModelFactory
-import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.enableSavedStateHandles
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
-import androidx.savedstate.SavedStateRegistry
-import androidx.savedstate.SavedStateRegistryController
-import androidx.savedstate.SavedStateRegistryOwner
 import java.util.concurrent.atomic.AtomicReference
 
 class AndroidScreenLifecycleOwner private constructor() :
     ScreenLifecycleOwner,
-    LifecycleOwner,
-    ViewModelStoreOwner,
-    SavedStateRegistryOwner,
-    HasDefaultViewModelProviderFactory {
-
-    override val lifecycle: LifecycleRegistry = LifecycleRegistry(this)
+    ViewModelStoreOwner {
 
     override val viewModelStore: ViewModelStore = ViewModelStore()
 
     private val atomicContext = AtomicReference<Context>()
 
-    private val controller = SavedStateRegistryController.create(this)
-
-    override val savedStateRegistry: SavedStateRegistry
-        get() = controller.savedStateRegistry
-
-    override val defaultViewModelProviderFactory: ViewModelProvider.Factory
-        get() = SavedStateViewModelFactory(
-            application = atomicContext.get()?.applicationContext?.getApplication(),
-            owner = this
-        )
-
-    override val defaultViewModelCreationExtras: CreationExtras
-        get() = MutableCreationExtras().apply {
-            val application = atomicContext.get()?.applicationContext?.getApplication()
-            if (application != null) {
-                set(ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY, application)
-            }
-            set(SAVED_STATE_REGISTRY_OWNER_KEY, this@AndroidScreenLifecycleOwner)
-            set(VIEW_MODEL_STORE_OWNER_KEY, this@AndroidScreenLifecycleOwner)
-
-            /* TODO if (getArguments() != null) {
-                extras.set<Bundle>(DEFAULT_ARGS_KEY, getArguments())
-            }*/
-        }
-
-    fun getViewModelCreationExtras(
-        extras: Bundle? = null
-    ) : CreationExtras {
-        return defaultViewModelCreationExtras.also {
-            MutableCreationExtras(it).apply {
-                if (extras != null) {
-                    set(DEFAULT_ARGS_KEY, extras)
-                }
-            }
-        }
-    }
-
-    init {
-        controller.performAttach()
-        enableSavedStateHandles()
-    }
+//    fun getViewModelCreationExtras(
+//        extras: Bundle? = null
+//    ) : CreationExtras {
+//        return MutableCreationExtras(it).apply {
+//                if (extras != null) {
+//                    set(DEFAULT_ARGS_KEY, extras)
+//                }
+//            }
+//        }
+//    }
 
     @Composable
     override fun ProvideBeforeScreenContent(
@@ -108,18 +55,10 @@ class AndroidScreenLifecycleOwner private constructor() :
 
         return remember(this) {
             listOf(
-                LocalViewModelStoreOwner provides this,
-                LocalSavedStateRegistryOwner provides this
+                LocalViewModelStoreOwner provides this
             )
         }
     }
-
-    private tailrec fun Context.getApplication(): Application? = when (this) {
-        is Application -> this
-        is ContextWrapper -> baseContext.getApplication()
-        else -> null
-    }
-
     companion object {
         fun get(screen: Screen): ScreenLifecycleOwner {
             return ScreenLifecycleStore.register(screen) { AndroidScreenLifecycleOwner() }
