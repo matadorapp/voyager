933e501477dd71d8be220ee1d9da9a13fc7166b2
Frederik J <>
2024-04-11 13:33:29 +0200

    Bumped version name

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val compile_sdk_version = 34
 
-    const val library_version = "1.0.7"
+    const val library_version = "1.0.8"
 
     const val gradle_version = "8.3.1"
     const val kotlin_version = "1.9.23"
