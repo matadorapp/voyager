b95832d8a9bffa160a653325dbe3fe5044e6fdf8
Frederik J <>
2025-05-26 12:44:05 +0200

    Bumped dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,21 +1,21 @@
 object Versions {
-    const val compile_sdk_version = 34
+    const val compile_sdk_version = 35
 
     const val library_version = "1.0.9"
 
-    const val gradle_version = "8.3.2"
-    const val kotlin_version = "2.0.21"
+    const val gradle_version = "8.10.0"
+    const val kotlin_version = "2.1.21"
 
-    const val ktx_version = "1.12.0"
+    const val ktx_version = "1.16.0"
 
     const val hilt_version = "2.51.1"
-    const val ksp_version = "2.0.21-1.0.26"
-    const val compose_plugin_version = "2.0.21"
+    const val ksp_version = "2.1.21-2.0.1"
+    const val compose_plugin_version = "2.1.21"
 
-    const val compose_version = "1.7.5"
-    const val compose_material_version = "1.3.1"
-    const val compose_activity_version = "1.9.3"
-    const val compose_lifecycle_runtime_version = "2.8.7"
+    const val compose_version = "1.8.2"
+    const val compose_material_version = "1.3.2"
+    const val compose_activity_version = "1.10.1"
+    const val compose_lifecycle_runtime_version = "2.9.0"
 
-    const val lifecycle_version = "2.8.7"
+    const val lifecycle_version = "2.9.0"
 }
